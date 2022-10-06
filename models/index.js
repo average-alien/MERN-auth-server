23a056ef97494d9caa94d0b4a9ed31cfd581a781
average-alien <allentang113@gmail.com>
2022-10-06 16:32:42 -0700

    finish auth lesson

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 const mongoose = require("mongoose")
 require('dotenv').config()
 
+// here is where you can update your database name
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/mernAuth'
 
 mongoose.connect(MONGODB_URI)
